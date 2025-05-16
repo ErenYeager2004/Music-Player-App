ef0917ec5aaf526bf3dd596a25175a4a69a69708
Debjit Saha <debjitsaha606@gmail.com>
2025-05-16 10:58:29 +0530

    gradle build modified

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.picasso)
+    implementation(libs.recyclerview)
+    implementation(libs.glide)
+    implementation(libs.androidx.media)
+
 }
